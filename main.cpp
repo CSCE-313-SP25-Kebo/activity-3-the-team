1a19672a8e189bb02e10a8ddf9c127380164e66e
vedatuzunoglu <124628211+vedatuzunoglu@users.noreply.github.com>
2025-01-30 16:54:48 -0600

    Update main.cpp for the activity
    Adjusted the code according to the instruction given.

--- main.cpp
+++ main.cpp
@@ -2,64 +2,52 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <csignal>
 
 using namespace std;
 
-int main(int argc, char *argv[])
-{
-    int option = 0; // default option: execute the command ls -l and terminate normally
+int main(int argc, char *argv[]) {
+    int option = 0;  // default option: execute the command ls -l and terminate normally
 
-    // Get flag from command line
+    // Parse command line arguments
     int opt;
-    while ((opt = getopt(argc, argv, "n:")) != -1)
-    {
-        switch (opt)
-        {
-        case 'n':
+    while ((opt = getopt(argc, argv, "n:")) != -1) {
+        if (opt == 'n') {
             option = atoi(optarg);
-            break;
         }
     }
 
-    /* TODO: FORK A NEW PROCESS */
+    pid_t pid = fork(); // Fork a new process
 
-    if (/* TODO: CONDITION IF FORK FAILS*/)
-    {
+    if (pid < 0) {
         cout << "Fork failed" << endl;
         return 1;
-    }
-    else if (/* TODO: CONDITION IF CHILD PROCESS */)
-    {
+    } else if (pid == 0) { // Child process
         cout << "Hello from the child process!" << endl;
-        /* TODO: PRINT THE PARENT PID value: "The parent process ID is $ID" */
+        cout << "The parent process ID is " << getppid() << endl;
 
-        if (option % 2 == 0) // if the option number is even, execute the command ls -l and terminate normally
-        {
-            std::cout << "The child process will execute the command: ls -l after 6 seconds" << std::endl;
-            /* TODO: SLEEP FOR 6 SECONDS*/
-            /* TODO: EXECUTE THE COMMAND ls -l USING EXECVP*/
+        if (option % 2 == 0) {
+            cout << "The child process will execute the command: ls -l after 6 seconds" << endl;
+            sleep(6); // Sleep for 6 seconds
+            char *args[] = {(char *)"ls", (char *)"-l", NULL};
+            execvp("ls", args); // Execute ls -l
+            _exit(EXIT_SUCCESS); // Exit after execution to avoid further execution of code
+        } else {
+            cout << "The child process is exiting" << endl;
+            kill(getpid(), SIGINT); // Send SIGINT to itself
         }
-        else // if the option number is odd, terminate with a kill signal
-        {
-            std::cout << "The child process is exiting" << std::endl;
-            kill(getpid(), SIGINT);
-        }
-    }
-    else if (/*TODO: CONDITION IF PARENT PROCESS*/)
-    {
+    } else { // Parent process
         int status;
-
-        /* TODO: WAIT FOR CHILD PROCESS TO FINISH */
+        waitpid(pid, &status, 0); // Wait for child process to finish
 
         cout << "\nHello from the parent process!" << endl;
+        cout << "The child process ID is " << pid << endl;
 
-        /* TODO: PRINT THE CHILD PID value: "The child process ID is $ID" */
-
-        /* TODO: PRINT THE EXIT STATUS OF THE CHILD PROCESS BASED waitpid().
-        MAKE SURE TO PASS BY REFERENCE THE STATUS VARIABLE TO THE SECOND PARAMETER OF waitpid()
-        IF WIFEXITED, PRINT THE MESSAGE "The child process exited normally" WITH ENDLINE
-        IF WIFSIGNALED, PRINT THE MESSAGE "The child process exited due to the kill signal" WITH ENDLINE
-        */
+        if (WIFEXITED(status)) {
+            cout << "The child process exited normally" << endl;
+        } else if (WIFSIGNALED(status)) {
+            cout << "The child process exited due to the kill signal" << endl;
+        }
     }
 
     return 0;
